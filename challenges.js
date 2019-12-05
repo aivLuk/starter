226bbb5426904a8b52d17319a37925f6253d76a3
Aivaras <lukauskasaivaras@gmail.com>
2019-12-05 10:00:16 +0100

    Updating new 2x6 rule

---
+++ challenges.js
@@ -0,0 +1,101 @@
+/*
+GAME RULES:
+
+Challenge 1 - if a player rolls 6 2x in a row, his total score goes to 0 and it's next players turn;
+
+*/
+
+var scores, roundScore, activePlayer;
+var gamePlaying = true;
+var lastDice;
+init();
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+    if (gamePlaying) {
+        // 1. Random dice number
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        // 2. Display the result
+        var diceDOM = document.querySelector(".dice");
+        diceDOM.style.display = "block";
+        diceDOM.src = "dice-" + dice + ".png";
+
+        // 3. Update the round score
+        if (dice === 6 && lastDice === 6) {
+            scores[activePlayer] = 0;
+            document.querySelector("#score-" + activePlayer).textContent = 0;
+            nextPlayer();
+        } else if (dice !== 1) {
+            // add score
+            roundScore += dice;
+            document.querySelector("#current-" + activePlayer).textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
+        lastDice = dice;
+    }
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+    if (gamePlaying) {
+        // Add CURRENT score to a GLOBAL score
+        scores[activePlayer] += roundScore;
+
+        // Update the UI
+        document.querySelector("#score-" + activePlayer).textContent =
+            scores[activePlayer];
+
+        // Check if a player won the game
+        if (scores[activePlayer] >= 100) {
+            document.querySelector("#name-" + activePlayer).textContent = "WINNER!";
+            document.querySelector(".dice").style.display = "none";
+            document
+                .querySelector(".player-" + activePlayer + "-panel")
+                .classList.add("winner");
+            document
+                .querySelector(".player-" + activePlayer + "-panel")
+                .classList.remove("active");
+            gamePlaying = false;
+
+        } else {
+            nextPlayer();
+        }
+    }
+});
+
+function nextPlayer() {
+    // Next players turn
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    document.querySelector(".dice").style.display = "none";
+}
+
+document.querySelector(".btn-new").addEventListener("click", init)
+
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector(".dice").style.display = "none";
+
+    document.getElementById("score-0").textContent = "0";
+    document.getElementById("score-1").textContent = "0";
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector("#name-0").textContent = "Player 1";
+    document.querySelector("#name-1").textContent = "Player 2";
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("winner");
+
+    document.querySelector(".player-0-panel").classList.remove("active");
+    document.querySelector(".player-1-panel").classList.remove("active");
+    document.querySelector(".player-0-panel").classList.add("active");
+}
